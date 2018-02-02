d0ba26ab1e01b519446a1b05f78a487a2a787f3c
dustin.yost <dustin.yost.t@gmail.com>
2018-02-01 20:13:10 -0500

    Add types to ship component scripts

---
+++ Skyrates/Assets/Scripts/Client/Ship/ShipBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBuilder : MonoBehaviour
+{
+
+    public enum ComponentType
+    {
+        Hull, Propulsion, Ammunition, Sail, Figurehead, Navigation,
+    }
+
+    public ShipHull hull;
+    public ShipComponent propulsion;
+    public ShipComponent ammunition;
+    public ShipComponent sail;
+    public ShipComponent figurehead;
+    public ShipComponent navigation;
+
+}
