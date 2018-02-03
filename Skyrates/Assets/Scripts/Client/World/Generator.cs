0f726ce870912d6333bb23c6bb3f5ea3732aec63
dustin.yost <dustin.yost.t@gmail.com>
2018-02-03 17:01:28 -0500

    Add scale

---
+++ Skyrates/Assets/Scripts/Client/World/Generator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Generator : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    public int amount;
+
+    void Awake()
+    {
+        //this.Generate();
+    }
+
+    public void Generate()
+    {
+        Bounds bounds = this.GetComponent<BoxCollider>().bounds;
+        for (int i = 0; i < this.amount; i++)
+        {
+            this.Generate(this.prefab, bounds);
+        }
+    }
+
+    private void Generate(GameObject prefab, Bounds bounds)
+    {
+        Vector3 pos = UnityEngine.Random.insideUnitSphere;
+        pos.Scale(bounds.size);
+        //pos += bounds.min;
+
+        Vector3 rot = new Vector3(0, UnityEngine.Random.value * 360, 0);
+
+        GameObject generated = Instantiate(prefab, this.transform);
+        generated.transform.SetPositionAndRotation(pos, Quaternion.Euler(rot));
+
+    }
+    
+}
