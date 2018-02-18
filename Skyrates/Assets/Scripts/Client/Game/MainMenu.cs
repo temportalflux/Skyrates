02029752c8e00dbae09268cf1c747a3172baed1a
dustin.yost <dustin.yost@mymail.champlain.edu>
2018-02-17 23:34:20 -0500

    Add end state

---
+++ Skyrates/Assets/Scripts/Client/Game/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Skyrates.Client.Game;
+using Skyrates.Common.Network;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartStandalone()
+    {
+        NetworkComponent.Instance.StartStandalone();
+    }
+
+}
