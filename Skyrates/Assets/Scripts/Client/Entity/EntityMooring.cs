342f1ee5f7b51e2de00d4d6c906071882001f90b
Summer Softleigh <Summer.Softleigh@mymail.champlain.edu>
2018-03-06 18:50:42 -0500

    Create mooring prefab and script

---
+++ Skyrates/Assets/Scripts/Client/Entity/EntityMooring.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Skyrates.Client.Entity
+{
+
+	/// <summary>
+	/// A static entity where you can upgrade your components.
+	/// </summary>
+	public class EntityMooring : Common.Entity.Entity
+	{
+
+	}
+}
