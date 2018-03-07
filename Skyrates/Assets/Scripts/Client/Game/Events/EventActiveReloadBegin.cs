2f02fa59ca0d8087408f4d604b93b9208475083e
dustin.yost <dustin.yost@mymail.champlain.edu>
2018-03-07 00:25:23 -0500

    Add active reload UI

---
+++ Skyrates/Assets/Scripts/Client/Game/Events/EventActiveReloadBegin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Skyrates.Client.Entity;
+using Skyrates.Common.Entity;
+using UnityEngine;
+
+namespace Skyrates.Client.Game.Event
+{
+    
+    public class EventActiveReloadBegin : EventEntityPlayerShip
+    {
+
+        public bool IsStarboard;
+
+        public float ActiveReloadStart;
+        public float ActiveReloadEnd;
+
+        public EventActiveReloadBegin(EntityPlayerShip playerShip,
+            bool isStarboard, float percentStart, float percentEnd)
+            : base(GameEventID.ActiveReloadBegin, playerShip)
+        {
+            this.IsStarboard = isStarboard;
+            this.ActiveReloadStart = percentStart;
+            this.ActiveReloadEnd = percentEnd;
+        }
+
+        public float GetPercentUpdate()
+        {
+            return this.IsStarboard
+                ? this.PlayerShip.PlayerData.StateData.ShootingDataStarboardPercentReloaded
+                : this.PlayerShip.PlayerData.StateData.ShootingDataPortPercentReloaded;
+        }
+
+    }
+
+}
---
+++ Skyrates/Assets/UIGameplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Skyrates.Client.Game;
+using Skyrates.Client.Game.Event;
+using UnityEngine;
+
+public class UIGameplay : MonoBehaviour
+{
+
+    public UIFillCutoffBar ActiveReloadStarboard;
+    public UIFillCutoffBar ActiveReloadPort;
+
+    void OnEnable()
+    {
+        GameManager.Events.ActiveReloadBegin += this.OnActiveReloadBegin;
+    }
+
+    void OnDisable()
+    {
+        GameManager.Events.ActiveReloadBegin -= this.OnActiveReloadBegin;
+    }
+
+    void OnActiveReloadBegin(GameEvent evt)
+    {
+        EventActiveReloadBegin evtReload = (EventActiveReloadBegin) evt;
+        UIFillCutoffBar reloadBar = evtReload.IsStarboard ? this.ActiveReloadStarboard : this.ActiveReloadPort;
+        reloadBar.Execute(evtReload.ActiveReloadStart, evtReload.ActiveReloadEnd, evtReload.GetPercentUpdate);
+    }
+
+}
